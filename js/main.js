5387aaeeed1e2d4e93c6ed38a98274a449a5a940
hyemin23 <hm_stom@naver.com>
2021-06-26 14:03:09 +0900

    #스크롤 css 수정중

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-alert(1)
+//즉시실행 함수
+(() => {
+    //기기에 따른 스크롤 section에 따른 배열 생성
+    const sceneInfo = [
+        {
+            tpye: "sticky",
+            heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 설정
+            scrollHeight: 0,
+        },
+        {
+            tpye: "normal",
+            heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 설정
+            scrollHeight: 0,
+        },
+        {
+            tpye: "sticky",
+            heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 설정
+            scrollHeight: 0,
+        },
+        {
+            tpye: "sticky",
+            heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 설정
+            scrollHeight: 0,
+        },
+    ];
+
+    function setLayout() {
+        //각 스크롤 섹션의 높이 셋팅
+
+    }
+
+
+})();
